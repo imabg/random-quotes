bc554db18f4963cf7e17c9a983d0f28c2ade58d5
abhay676 <abhaygoswami676@gmail.com>
2018-11-03 21:59:17 +0530

    'first-commit'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Random Quotes</h2>
+    <rndmQuote :myQuote = "quote"
+      :renderQuote = "renderQuote"
+    ></rndmQuote>
+    <button class="btn btn-outline-success" style="margin: 25px" @click = "generateQuote">Quote</button>
+    <button class="btn btn-primary" style="margin: 25px" v-if = "renderQuote" @click = "tweetMe"><i class="fab fa-twitter"> Tweet</i></button>
+    <div class="alert alert-primary" role="alert">
+        Crafted By: <a href="https://github.com/abhay676" target="_blank">Abhay Goswami</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import rndmQuote from './components/renderQuote.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    rndmQuote
+  },
+  data() {
+    return {
+      quote: '',
+      renderQuote: false
+    }
+  },
+  methods: {
+    generateQuote() {
+      axios
+      .get('https://talaikis.com/api/quotes/random/')
+      .then(response =>{
+        this.quote = response;
+        });
+        this.renderQuote = true;
+    },
+    tweetMe() {
+      let query = this.quote.data.quote;
+       window.open(`https://twitter.com/intent/tweet?text=${query}`);
+    }
   }
 }
 </script>
@@ -25,4 +53,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h2 {
+  font-size: 55px;
+}
+
 </style>
---
+++ src/components/renderQuote.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div v-if = "renderQuote">
+    <div class="cat">
+      <label >Category: </label>
+      <span> {{myQuote.data.cat}}</span>
+    </div>
+    <div class="quote">
+      <span id="rndmQuote">{{myQuote.data.quote}}</span>
+      <div class="author">
+        <label>By {{myQuote.data.author}}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    myQuote: {
+      required: true
+    },
+    renderQuote: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .cat {
+    font-weight: bold;
+    font-size: 15px;
+    margin-top: 22px;
+    text-align: right;
+  }
+  .quote {
+    border: 1px solid black;
+    box-shadow: 1px 2px 2px #ccc;
+    margin: auto;
+  }
+  #rndmQuote {
+    font-family: 'Pattaya', sans-serif;
+    font-size: 25px;
+  }
+  .author{
+    font-family: 'Cedarville Cursive', cursive;
+    font-size: 25px;
+  }
+</style>
